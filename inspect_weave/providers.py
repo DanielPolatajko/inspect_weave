5ce25779b9e34f2e11196e68e59d8328fc289768
Matan Shtepel <matan.shtepel@gmail.com>
2025-08-06 13:24:27 -0400

    Revert "Add wandb models logging (#30)"
    This reverts commit 8ccb4ba73e135e63b8c20d6168bb30b552aa5953.

--- inspect_weave/providers.py
+++ inspect_weave/providers.py
@@ -1,10 +1,7 @@
+from inspect_weave.hooks import WeaveEvaluationHooks
 from inspect_ai.hooks import hooks
-from inspect_weave.hooks import WeaveEvaluationHooks, WandBModelHooks
 
-@hooks(name="weave_evaluation_hooks", description="Weave evaluation integration")
+
+@hooks(name="weave_evaluation_hooks", description="Integration hooks for writing evaluation results to Weave")
 def weave_evaluation_hooks():
     return WeaveEvaluationHooks
-
-@hooks(name="wandb_models_hooks", description="Weights & Biases model integration")
-def wandb_models_hooks():
-    return WandBModelHooks
